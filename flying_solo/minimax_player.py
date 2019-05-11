3b919386f68168cfbeece40d3cbcd30bf50a8d28
James Taranto <tarantoj@student.unimelb.edu.au>
2019-05-11 16:38:10 +1000

    minimax psuedocode

--- flying_solo/minimax_player.py
+++ flying_solo/minimax_player.py
@@ -1,5 +1,19 @@
 from flying_solo.player import Player
 
+"""
+ Inputs: State s, Player ID p
+ Output: (utility vector, best action)
+ function MAXN(s, p):
+    if CUTOFF-TEST(s):
+        return (EVALUATE(s), no-action)
+    v-max  <- (-inf, -inf, ..., -inf) # n dimensions
+    best-a <- no-action
+    for each Action a in ACTIONS(s):
+        (v, *) <- MAXN(RESULT(s, a), NEXT(p))
+        if v[p] > v-max[p]:
+            v-max  <- vbest-a <- areturn (v-max, best-a)
+"""
+
 
 class Node:
     def __init__(self, state, parent):
