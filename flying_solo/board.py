84f6e0c5f5c43b2da916bfa9d14389c6b4e05c79
James Taranto <tarantoj@student.unimelb.edu.au>
2019-05-15 12:25:28 +1000

    abstract board utility functions into module

---
+++ flying_solo/board.py
@@ -0,0 +1,78 @@
+class Board:
+    colours = ["red", "green", "blue"]
+    STARTING_HEXES = {
+        "red": {(-3, 3), (-3, 2), (-3, 1), (-3, 0)},
+        "green": {(0, -3), (1, -3), (2, -3), (3, -3)},
+        "blue": {(3, 0), (2, 1), (1, 2), (0, 3)},
+    }
+    FINISHING_HEXES = {
+        "red": {(3, -3), (3, -2), (3, -1), (3, 0)},
+        "green": {(-3, 3), (-2, 3), (-1, 3), (0, 3)},
+        "blue": {(-3, 0), (-2, -1), (-1, -2), (0, -3)},
+    }
+    ADJACENT_STEPS = [(-1, +0), (+0, -1), (+1, -1), (+1, +0), (+0, +1), (-1, +1)]
+    MAX_TURNS = 256  # per player
+    ran = range(-3, +3 + 1)
+    hexes = {(q, r) for q in ran for r in ran if -q - r in ran}
+
+    def __init__(self):
+        self.board = {qr: " " for qr in Board.hexes}
+        for c in Board.colours:
+            for qr in Board.STARTING_HEXES[c]:
+                self.board[qr] = c
+
+    @staticmethod
+    def available_actions(board, colour):
+        """Return available actions for a colour on a board
+        
+        Arguments:
+            board {dict} -- dictionary representing board state
+            colour {string} -- string representing player
+        
+        Returns:
+            list -- Available actions
+        """
+        available_actions = []
+        for qr in Board.hexes:
+            if board[qr] == colour:
+                if qr in Board.FINISHING_HEXES[colour]:
+                    available_actions.append(("EXIT", qr))
+                q, r = qr
+                for dq, dr in Board.ADJACENT_STEPS:
+                    for i, atype in [(1, "MOVE"), (2, "JUMP")]:
+                        tqr = q + dq * i, r + dr * i
+                        if tqr in Board.hexes:
+                            if board[tqr] == " ":
+                                available_actions.append((atype, (qr, tqr)))
+                                break
+        if not available_actions:
+            available_actions.append(("PASS", None))
+        return available_actions
+
+    @staticmethod
+    def apply_action(score, board, colour, action):
+        """Applies an action to a board
+        
+        Arguments:
+            score {dict} -- dictionary of scores
+            board {dict} -- dictionary representing board state
+            colour {string} -- player to apply action to
+            action {tuple} -- action to apply
+        """
+        atype, aargs = action
+        if atype == "MOVE":
+            qr_a, qr_b = aargs
+            board[qr_a] = " "
+            board[qr_b] = colour
+        elif atype == "JUMP":
+            qr_a, qr_b = (q_a, r_a), (q_b, r_b) = aargs
+            qr_c = (q_a + q_b) // 2, (r_a + r_b) // 2
+            board[qr_a] = " "
+            board[qr_b] = colour
+            board[qr_c] = colour
+        elif atype == "EXIT":
+            qr = aargs
+            board[qr] = " "
+            score[colour] += 1
+        else:  # atype == "PASS":
+            pass
